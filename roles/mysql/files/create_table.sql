a137bbb644e5134703a9e479213a423df7d4267a
Nir Tal <nirtal@outlook.com>
2017-02-16 22:35:41 +0000

    Added mysql role

---
+++ roles/mysql/files/create_table.sql
@@ -0,0 +1,3 @@
+USE lab;
+CREATE TABLE users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,first_name VARCHAR (15),last_name VARCHAR (20));
+INSERT INTO users (id,first_name,last_name) VALUES (NULL, 'Nir','Tal');
